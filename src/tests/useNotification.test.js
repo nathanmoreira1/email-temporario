d822c6058d575fc5d761befcf7709597fee8a233
Nathan Moreira de Macedo <nathanmoreirademacedo@gmail.com>
2023-02-20 12:12:31 -0300

    remote tests

--- src/tests/useNotification.test.js
+++
@@ -1,9 +0,0 @@
-import { renderHook } from "@testing-library/react-hooks";
-import useNotification from "../hooks/useNotification";
-
-describe("useNotification hook", () => {
-  test("askNotificationPermission returns a promise", async () => {
-    const { result } = renderHook(() => useNotification());
-    expect(result.current.askNotificationPermission()).toBeInstanceOf(Promise);
-  });
-});
